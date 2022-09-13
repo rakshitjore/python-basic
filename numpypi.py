a15c848e2094a57336f1b857dbf9205ac586e9ed
Rakshit Jore <jorerakshit@gmail.com>
2022-09-14 01:06:51 +0530

    Add files via upload

---
+++ numpypi.py
@@ -0,0 +1,9 @@
+import numpy as np 
+a1d=np.array([1,2,3])
+a2d=np.array([[1,2],[3,4]])
+a3d=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(a1d)
+print(a2d)
+print(a3d)
+print(np.arange(10))
+print(np.arange(2,3,0.1))
